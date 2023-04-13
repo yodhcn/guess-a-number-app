fdddeb0bc12fdff18e61497433a452c00ac5da6f
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-13 17:49:33 +0800

    4.6. Styling a View as a Card Container (with Drop Shadows & Rounded Corners)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -31,6 +31,16 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
+    backgroundColor: "white",
+    shadowColor: "black",
+    // iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.6,
+    // Android
+    elevation: 5,
+    padding: 20,
+    borderRadius: 10,
   },
   buttonConatiner: {
     flexDirection: "row",
