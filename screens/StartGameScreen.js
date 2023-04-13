95d070ed94cd1e75e18ac1c6bf0a1d157564a1c6
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-13 21:26:04 +0800

    4.12. Cleaning User Input & Controlling the Soft Keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,31 +1,48 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from "../constants/colors";
 
 export default function StartGameScreen(props) {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  function unmberInputHandler(inputText) {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
-      <Card style={styles.inputConatiner}>
-        <Text>Select a Number</Text>
-        <Input
-          style={styles.input}
-          blurOnSubmit
-          keyboardType="number-pad"
-          maxLength={2}
-        />
-        <View style={styles.buttonConatiner}>
-          <View style={styles.button}>
-            <Button title="Reset" color={Colors.primary} onPress={() => {}} />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputConatiner}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            keyboardType="number-pad"
+            maxLength={2}
+            value={enteredValue}
+            onChangeText={unmberInputHandler}
+          />
+          <View style={styles.buttonConatiner}>
+            <View style={styles.button}>
+              <Button title="Reset" color={Colors.primary} onPress={() => {}} />
+            </View>
+            <View style={styles.button}>
+              <Button title="Confirm" color={Colors.accent} />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button title="Confirm" color={Colors.accent} />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
