178814e800383df0fedfe49d5014dc30f131a8ff
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-14 15:41:31 +0800

    4.26. Styling Images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,7 +8,12 @@ export default function StartGameScreen(props) {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <Image source={require("../assets/images/success.png")} />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
       <BodyText>Number of rounds: {props.roundNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="New Game" onPress={props.onRestart} />
@@ -22,5 +27,18 @@ const styles = StyleSheet.create({
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
