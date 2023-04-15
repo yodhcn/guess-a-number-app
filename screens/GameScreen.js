1bb9e9a2b560778a2791cf9c706b40b6f92c9cf7
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-15 15:40:24 +0800

    5.5. Using Dimensions in if Checks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, StyleSheet, Alert, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/NumberContainer";
@@ -68,6 +75,11 @@ export default function GameScreen(props) {
     setPastGuesses((curPastGuesses) => [nextNumber, ...curPastGuesses]);
   }
 
+  let listContainerStyle = (listContainerStyle = styles.listContainer);
+  if (Dimensions.get("window").width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -80,7 +92,7 @@ export default function GameScreen(props) {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         <FlatList
           contentContainerStyle={styles.list}
           data={pastGuesses}
@@ -102,13 +114,17 @@ const styles = StyleSheet.create({
   buttonConatiner: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
     flex: 1,
     width: "60%",
+  },
+  listContainerBig: {
+    flex: 1,
+    width: "80%",
   },
   list: {
     flexGrow: 1,
