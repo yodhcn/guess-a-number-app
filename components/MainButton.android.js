8c930ba812b349d8289f7b8fbc07c33bd9cb288a
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-16 15:02:27 +0800

    5.17. Using Platform-specific Code Files

--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -12,7 +12,7 @@ import Colors from "../constants/colors";
 
 export default function MainButton(props) {
   let ButtonComponent = TouchableOpacity;
-  if (Platform.OS === "android" && Platform.Version >= 21) {
+  if (Platform.Version >= 21) {
     ButtonComponent = TouchableNativeFeedback;
   }
   return (
